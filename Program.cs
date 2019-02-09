f00702ad915423263a0076788e0be2d4b7349d69
osantiago <orlando.santiago193@yahoo.com>
2019-02-08 20:19:22 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CountingValleys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string path = "UDDDUDUUDUDU";
+
+            int altitude = 0;
+            int count = 0;
+
+            foreach (var item in path)
+            {
+                if (item == 'U')
+                {
+                    altitude++;
+                    if (altitude == 0)
+                    {
+                        count++;
+                    }
+                }
+                else
+                {
+                    altitude--;
+                }
+
+            }
+
+            Console.WriteLine(count);
+
+        }
+    }
+}
